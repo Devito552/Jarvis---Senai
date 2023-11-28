99b632d72c5fb9f843ebe40b03fa4e5225a21044
Guilherme Devito <117872245+Devito552@users.noreply.github.com>
2023-11-28 13:12:18 -0300

    Aula 2

--- index.js
+++ index.js
@@ -31,3 +31,71 @@ document.addEventListener('DOMContentLoaded', function () {
         toggleButton.innerHTML = '<i class="fas fa-moon"></i>';
     }
 });
+
+
+// API OpenAI
+
+const ConsultarOpenAI = async(pergunta) => {
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Bearer Chave API OpenAI");
+myHeaders.append("Cookie", "__cf_bm=kMUEoHjtMzklj3W_GajmYdHLQgI6vkP.gN8RFW0vmRo-1701178894-0-AezxRH5TomZBEiVpbGUik8o9S8Nx0Y9fDS4i5jzNaNHLrTYvvpq5thdeUzys9xdDiZNhW5yPGdRBS5MsmfqLFKg=; _cfuvid=ZvHhSFIW0uRoFz_8gm8f9Ot31a_omOQczXppe7jYemA-1701178894004-0-604800000");
+
+var raw = JSON.stringify({
+  "model": "ft:gpt-3.5-turbo-0613:zeros-e-um::8PrTlJrT",
+  "messages": [
+    {
+      "role": "system",
+      "content": "Jarvis é um chatbot pontual e muito simpático que ajuda as pessoas"
+    },
+    {
+      "role": "user",
+      "content": pergunta
+    }
+  ],
+  "temperature": 0.2
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://api.openai.com/v1/chat/completions", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result.choices[0].message.content))
+  .catch(error => console.log('error', error));
+
+}
+
+// Capturar voz do navegador e transcrever
+
+const CapturarVoz = () => {
+
+    var startButton = document.getElementById('capture');
+    var resultElement = document.getElementById('prompt');
+
+    var recognition = new webkitSpeechRecognition();
+
+    recognition.lang = window.navigator.language;
+    recognition.interimResults = true;
+
+    startButton.addEventListener('click', () => { recognition.start(); });
+
+
+        recognition.addEventListener('result', (event) => {
+            const result = event.results[event.results.length - 1][0].transcript;
+            resultElement.value = result;
+    });
+
+    recognition.addEventListener('end', () => {
+        const textoCapturado = resultElement.value;
+        ConsultarOpenAI(textoCapturado);
+
+    });
+}
+
+CapturarVoz();
